2a1295cba6a51e74b17841f37d4753353a1214d0
Dmitry Fiodorov <fedor.dmitry@gmail.com>
2022-10-31 11:03:02 +0300

    type descriptor

---
+++ include/d4f__type_descriptor.h
@@ -0,0 +1,18 @@
+#ifndef d4f__type_descriptor_h
+#define d4f__type_descriptor_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    extern void* d4f__TypeDescriptor;
+
+    extern void* d4f__new(const void* type, ...);
+    extern void d4f__delete(void* self);
+    extern void* d4f__clone(const void* self);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !d4f__type_descriptor_h */
---
+++ src/d4f__type_descriptor.c
@@ -0,0 +1,42 @@
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include "d4f__type_descriptor.h"
+
+typedef struct TypeDescriptor {
+    size_t size;
+    void* (*ctor)(void* self, va_list* args);
+    void (*dtor)(void* self);
+    void* (*clone)(const void* self);
+    int (*compare)(const void* a, const void* b);
+} TypeDescriptor;
+
+void* d4f__new(const void* type, ...) {
+    assert(type != NULL);
+
+    TypeDescriptor* td = (TypeDescriptor*)type;
+
+    void* new_object = malloc(td->size);
+    assert(new_object != NULL);
+
+    *(TypeDescriptor**)new_object = td;
+
+    if (td->ctor != NULL) {
+        va_list args;
+        va_start(args, type);
+        td->ctor(new_object, &args);
+        va_end(args);
+    }
+
+    return new_object;
+}
+
+void d4f__delete(void* self) {
+    TypeDescriptor* td = *(TypeDescriptor**)self;
+
+    if (td->dtor != NULL) {
+        td->dtor(self);
+    }
+
+    free(self);
+}
